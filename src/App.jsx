cc0b5e7bf8d9f6e415e54d01533780c6e50cd273
Sami Kalammallah <skalexsong@gmail.com>
2023-10-19 22:25:24 +0700

    chore: create first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Button from "./components/atoms/Button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,9 +15,9 @@ function App() {
       </div>
       <h1 className="text-red-500">Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
